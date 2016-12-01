19d037e6d8853bdbfda266769eb06887f64768e5
Pete Russell <pinkbatz@gmail.com>
2016-12-01 00:04:38 -0800

    Adds processors for wind dir, variable winds, vis, rvr (+tests).

--- rest-api/wxdecoder/metars/workers/metar_decoder.py
+++ rest-api/wxdecoder/metars/workers/metar_decoder.py
@@ -18,16 +18,25 @@ class MetarDecoder:
     # stores the decoded result in the relevant class field, strips the tokens
     # from the METAR and returns the result
 
-    # If the token doesn't match what it expects we can assume it's been omitted
-    # from the METAR and just return from the processor without doing anything.
+    # If the token doesn't match what it each processor expects we can assume
+    # it's been omitted from the METAR and return immediately.
     tokens = self.process_metar_header(tokens)
     tokens = self.process_iaao_id(tokens)
+    tokens = self.process_datetime(tokens)
+    tokens = self.process_automated_obs_flag(tokens)
+    tokens = self.process_wind_dir_speed(tokens)
+    tokens = self.process_wind_dir_varation(tokens)
+    tokens = self.process_visibility(tokens)
+    tokens = self.process_rvr(tokens)
 
     # Return any remaining tokens
     return tokens # tmp - not sure what we want to return here
 
   def process_metar_header(self, tokens):
     if tokens[0] == 'METAR':
+      tokens.pop(0)
+    elif tokens[0] == 'SPECI':
+      self.is_special_report = True
       tokens.pop(0)
     return tokens
 
@@ -46,13 +55,35 @@ class MetarDecoder:
       tokens.pop(0)
     return tokens
 
-  # METAR components. See http://www.met.tamu.edu/class/metar/quick-metar.html.
+  def process_wind_dir_speed(self, tokens):
+    if "KT" in tokens[0]:
+      self.wind_dir_speed = tokens.pop(0)
+    return tokens
+
+  def process_wind_dir_variation(self, tokens):
+    if "V" in tokens[0]:
+      self.wind_dir_variation = tokens.pop(0)
+    return tokens
+
+  def process_visibility(self, tokens):
+    if tokens[0].endswith('SM'):
+      self.vis = tokens.pop(0)
+    return tokens
+
+  def process_rvr(self, tokens):
+    if tokens[0].startswith('R') and tokens[0].endswith('FT'):
+      self.rvr = tokens.pop(0)
+    return tokens
+
+  # METAR components. See http://www.met.tamu.edu/class/metar/quick-metar.html
+  is_special_report = False
   icao_id = ''
   obs_datetime = None
   mod_auto = False
-  wind_dir_speed = ''
-  vis = ''
-  rvr = ''
+  wind_dir_speed = '' # TODO: this should probably be an object
+  wind_dir_variation = '' # TODO this should be an attribute in he wind_dir_speed object
+  vis = '' # TODO: could be an object (can include 'M' prefix to indicate <1/4SM)
+  rvr = '' # TODO: could be an object, can include modifiers which we may want to store
   wx_phenomena = []
   skc = []
   temp = ''
--- rest-api/wxdecoder/metars/workers/tests/token_processor_tests.py
+++ rest-api/wxdecoder/metars/workers/tests/token_processor_tests.py
@@ -29,30 +29,119 @@ class TestTokenProcessor:
     tokens = 'KHIO 290653Z'.split()
     res = decoder.process_icao_id(tokens)
     assert_equals(decoder.icao_id, 'KHIO')
-    assert_equals(tokens[0], '290653Z')
+    assert_equals(res[0], '290653Z')
 
   def test_process_datetime(self):
     decoder = MetarDecoder()
     tokens = '290653Z AUTO'.split()
     res = decoder.process_datetime(tokens)
     assert_equals(decoder.obs_datetime, '290653Z')
-    assert_equals(tokens[0], 'AUTO')
+    assert_equals(res[0], 'AUTO')
 
-  def test_automated_obs_flag_with_flag(self):
+  def test_process_automated_obs_flag_with_flag(self):
     decoder = MetarDecoder()
     tokens = 'AUTO 00000KT'.split()
     res = decoder.process_automated_obs_flag(tokens)
     assert_equals(decoder.mod_auto, True)
-    assert_equals(tokens[0], '00000KT')
+    assert_equals(res[0], '00000KT')
 
-  def test_automated_obs_flag_without_flag(self):
+  def test_process_automated_obs_flag_without_flag(self):
     ### If the AUTO flag is missing from the METAR, the obs_datetime
     ### field should be false and nothing stripped from the METAR
     decoder = MetarDecoder()
     tokens = '290653Z 00000KT'.split()
     res = decoder.process_automated_obs_flag(tokens)
     assert_equals(decoder.mod_auto, False)
-    assert_equals(tokens, ['290653Z', '00000KT'])
+    assert_equals(res, ['290653Z', '00000KT'])
+
+  def test_process_winds_calm(self):
+    decoder = MetarDecoder()
+    tokens = '00000KT 10SM'.split()
+    res = decoder.process_wind_dir_speed(tokens)
+    assert_equals(decoder.wind_dir_speed, '00000KT')
+    assert_equals(res, ['10SM'])
+
+  def test_process_winds_light_variable(self):
+    decoder = MetarDecoder()
+    tokens = 'VRB005KT'.split()
+    res = decoder.process_wind_dir_speed(tokens)
+    assert_equals(decoder.wind_dir_speed, 'VRB005KT')
+
+  def test_process_winds_basic(self):
+    decoder = MetarDecoder()
+    tokens = '18004KT 10SM'.split()
+    res = decoder.process_wind_dir_speed(tokens)
+    assert_equals(decoder.wind_dir_speed, '18004KT')
+
+  def test_process_winds_with_gusts(self):
+    decoder = MetarDecoder()
+    tokens = '21016G24KT 10SM'.split()
+    res = decoder.process_wind_dir_speed(tokens)
+    assert_equals(decoder.wind_dir_speed, '21016G24KT')
+
+  def test_process_winds_variable(self):
+    decoder = MetarDecoder()
+    tokens = '18015KT 150V210 10SM'.split()
+    # Make sure both the wind info and direction variation get picked up
+    res = decoder.process_wind_dir_speed(tokens)
+    res = decoder.process_wind_dir_variation(res)
+    assert_equals(decoder.wind_dir_speed, '18015KT')
+    assert_equals(decoder.wind_dir_variation, '150V210')
+
+  def test_process_winds_variable_missing(self):
+    ### If the variable winds section is missing we shouldn't set the
+    ### MetarDecoder's wind_dir_variation field, or strip anything
+    decoder = MetarDecoder()
+    tokens = '18015KT 10SM'.split()
+    # Include the wind_dir_speed processor for a sanity check
+    res = decoder.process_wind_dir_speed(tokens)
+    res = decoder.process_wind_dir_variation(res)
+    assert_equals(decoder.wind_dir_speed, '18015KT')
+    assert_equals(decoder.wind_dir_variation, '')
+    assert_equals(res, ['10SM'])
+
+  def test_process_visibility(self):
+    decoder = MetarDecoder()
+    tokens = '10SM R11/P6000FT'.split()
+    res = decoder.process_visibility(tokens)
+    assert_equals(decoder.vis, '10SM')
+    assert_equals(res[0], 'R11/P6000FT')
+
+  def test_process_visibility_missing(self):
+    ### If the visibility field is missing, the MetarDecoder's 'vis' field
+    ### should remain unset and nothing stripped from the METAR
+    decoder = MetarDecoder()
+    tokens = '21016G24KT R11/P6000FT'.split()
+    res = decoder.process_visibility(tokens)
+    assert_equals(decoder.vis, '')
+    assert_equals(res, ['21016G24KT', 'R11/P6000FT'])
+
+  def test_process_rvr(self):
+    decoder = MetarDecoder()
+    tokens = 'R11/P6000FT -RA BR'.split()
+    res = decoder.process_rvr(tokens)
+    assert_equals(decoder.rvr, 'R11/P6000FT')
+    assert_equals(tokens, ['-RA', 'BR'])
+
+  def test_process_rvr_missing(self):
+    ### If the RVR is missing, the MetarDecoder's 'rvr' field should
+    ### remain unset and nothing stripped from the METAR
+    decoder = MetarDecoder()
+    tokens = 'VRB005 -RA BR'.split()
+    res = decoder.process_rvr(tokens)
+    assert_equals(decoder.rvr, '')
+    assert_equals(tokens, ['VRB005', '-RA', 'BR'])
+
+  def test_process_rvr_missing_with_similar_wx_phenomena(self):
+    ### The RVR field begins with 'R', but the following WX phenomena
+    ### section can also start with 'R' in the case of moderate rain: 'RA'.
+    ### This test ensures we don't mis-identify 'RA' as the RVR field.
+    decoder = MetarDecoder()
+    tokens = '18015KT RA'.split()
+    res = decoder.process_wind_dir_speed(tokens)
+    res = decoder.process_rvr(res)
+    assert_equals(decoder.rvr, '')
+    assert_equals(tokens, ['RA'])
 
   @classmethod
   def teardown_class(cls):
