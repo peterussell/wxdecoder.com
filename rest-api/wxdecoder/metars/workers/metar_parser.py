e5f466867ce5b557a2945da21b2863a7808c7108
Pete Russell <pinkbatz@gmail.com>
2016-12-04 20:59:43 -0800

    Refactor, new layaout Controller > Parser > Decoder.

---
+++ rest-api/wxdecoder/metars/workers/metar_parser.py
@@ -0,0 +1,170 @@
+class MetarParser:
+
+  def parse_metar(self, raw_metar):
+    tokens = raw_metar.split()
+    tokens = self.parse_tokens(tokens)
+    return raw_metar
+
+  def parse_tokens(self, tokens):
+    # Each parseor gets called in turn. If the next token of the METAR matches
+    # what that parseor expects, it parses the relevant section (or sections),
+    # stores the parsed result in the relevant class field, strips the token(s)
+    # from the METAR and returns the result
+
+    # If the token doesn't match what it each parseor expects we can assume
+    # it's been omitted from the METAR and return immediately.
+    tokens = self.parse_metar_header(tokens)
+    tokens = self.parse_icao_id(tokens)
+    tokens = self.parse_datetime(tokens)
+    tokens = self.parse_automated_obs_flag(tokens)
+    tokens = self.parse_wind_dir_speed(tokens)
+    tokens = self.parse_wind_dir_variation(tokens)
+    tokens = self.parse_visibility(tokens)
+    tokens = self.parse_rvr(tokens)
+    tokens = self.parse_wx_phenomena(tokens)
+    tokens = self.parse_sky_condition(tokens)
+    tokens = self.parse_temp_dewpoint(tokens)
+    tokens = self.parse_altimeter(tokens)
+
+    # Store any remaining tokens as remarks (for now)
+    self.parsed_metar["remarks"] = ' '.join(tokens)
+
+    # Return any remaining tokens
+    return tokens # tmp - not sure what we want to return here
+
+  def parse_metar_header(self, tokens):
+    if tokens[0] == 'METAR':
+      tokens.pop(0)
+    elif tokens[0] == 'SPECI':
+      self.parsed_metar["is_special_report"] = True
+      tokens.pop(0)
+    return tokens
+
+  def parse_icao_id(self, tokens):
+    self.parsed_metar["icao_id"] = tokens.pop(0)
+    return tokens
+
+  def parse_datetime(self, tokens):
+    if tokens[0].endswith('Z'):
+      self.parsed_metar["obs_datetime"] = tokens.pop(0)
+    return tokens
+
+  def parse_automated_obs_flag(self, tokens):
+    if tokens[0] == 'AUTO':
+      self.parsed_metar["mod_auto"] = True
+      tokens.pop(0)
+    return tokens
+
+  def parse_wind_dir_speed(self, tokens):
+    if "KT" in tokens[0]:
+      self.parsed_metar["wind_dir_speed"] = tokens.pop(0)
+    return tokens
+
+  def parse_wind_dir_variation(self, tokens):
+    if "V" in tokens[0]:
+      self.parsed_metar["wind_dir_variation"] = tokens.pop(0)
+    return tokens
+
+  def parse_visibility(self, tokens):
+    if tokens[0].endswith('SM'):
+      self.parsed_metar["vis"] = tokens.pop(0)
+    return tokens
+
+  def parse_rvr(self, tokens):
+    if tokens[0].startswith('R') and tokens[0].endswith('FT'):
+      self.parsed_metar["rvr"] = tokens.pop(0)
+    return tokens
+
+  def parse_wx_phenomena(self, tokens):
+    wx_tokens = []
+    for t in tokens:
+      # Loop until we get a sky condition token.
+      # Note: this will break if the sky condition is omitted (TODO, revisit).
+      # One solution would be implement 'is_wx_phenomena_token(token)', but that
+      # could be expensive given how many tokens there are; it's cheaper to
+      # check the much smaller sky condition list.
+      if not self.is_sky_condition_token(t):
+        wx_tokens.append(t)
+      else:
+        break
+    # Remove the tokens from the original tokens list (we avoid doing this
+    # while iterating the original list).
+    for wxt in wx_tokens:
+      tokens.remove(wxt)
+    self.parsed_metar["wx_phenomena"] = list(wx_tokens)
+    return tokens
+
+  def parse_sky_condition(self, tokens):
+    sky_con_tokens = []
+    for t in tokens:
+    # Loop until we _don't_ get a sky condition token.
+      if self.is_sky_condition_token(t):
+        sky_con_tokens.append(t)
+      else:
+        break
+    # Remove tokens from the original tokens list
+    for sct in sky_con_tokens:
+      tokens.remove(sct)
+    self.parsed_metar["sky_condition"] = list(sky_con_tokens)
+    return tokens
+
+  def parse_temp_dewpoint(self, tokens):
+    if '/' in tokens[0]:
+      parts = tokens.pop(0).split('/')
+      self.parsed_metar["temp"] = parts[0]
+      self.parsed_metar["dewpoint"] = parts[1]
+    return tokens
+
+  def parse_altimeter(self, tokens):
+    if tokens[0].startswith('A'):
+      self.parsed_metar["altimeter"] = tokens.pop(0)
+    return tokens
+
+  ### Helpers - TODO: Move to utils module
+  def is_sky_condition_token(self, token):
+    if token.startswith('CLR') or \
+       token.startswith('FEW') or \
+       token.startswith('SCT') or \
+       token.startswith('BKN') or \
+       token.startswith('OVC'):
+      return True
+    return False
+
+  def __init__(self):
+    # Set default METAR components. See http://www.met.tamu.edu/class/metar/quick-metar.html
+    self.parsed_metar = {
+      "is_special_report": False,
+      "icao_id": '',
+      "obs_datetime": None,
+      "mod_auto": False,
+      "wind_dir_speed": '', # TODO: this should probably be an object
+      "wind_dir_variation": '', # TODO this should be an attribute in he wind_dir_speed object
+      "vis": '', # TODO: could be an object (can include 'M' prefix to indicate <1/4SM)
+      "rvr": '', # TODO: could be an object, can include modifiers which we may want to store
+      "wx_phenomena": [],
+      "sky_condition": [],
+      "temp": '',
+      "dewpoint": '',
+      "altimeter": '',
+      "remarks": '',
+      # This is all in the Remarks section, implementation coming to a future version near you.
+      # "tornadic_activity": []
+      # "stn_type": ''
+      # "peak_wind": ''
+      # "wind_shift": ''
+      # "variable_vis": ''
+      # "vis_second_loc": ''
+      # "lightning": []
+      # "precip_ts": []
+      # "virga": ''
+      # "variable_ceilingg": ''
+      # "ceiling_second_loc": ''
+      # "press_rise_fall_rapid": ''
+      # "sea_level_press": ''
+      # "hourly_precip": ''
+      # "three_six_hour_precip": []
+      # "twenty_four_hour_precip": []
+      # "press_tendency": ''
+      # "sensor_status": []
+      # "maint_reqd": ''
+    }
---
+++ rest-api/wxdecoder/metars/workers/tests/metar_parser_tests.py
@@ -0,0 +1,223 @@
+from nose import with_setup
+from nose.tools import assert_equals
+
+from metars.workers.metar_parser import MetarParser
+
+class TestTokenProcessor:
+
+  @classmethod
+  def setup_class(cls):
+    pass
+
+  ### Test full token parseor
+  def test_parse_metar_khio(self):
+    metar = 'METAR KHIO 290653Z AUTO 00000KT 10SM FEW032 OVC041 06/05 ' \
+            'A3017 RMK AO2 RAB35E44 SLP219 P0000 T00560050'
+    parser = MetarParser()
+    res = parser.parse_tokens(metar.split())
+    assert_equals(parser.parsed_metar["is_special_report"], False)
+    assert_equals(parser.parsed_metar["icao_id"], 'KHIO')
+    assert_equals(parser.parsed_metar["obs_datetime"], '290653Z')
+    assert_equals(parser.parsed_metar["mod_auto"], True)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '00000KT')
+    assert_equals(parser.parsed_metar["vis"], '10SM')
+    assert_equals(parser.parsed_metar["wx_phenomena"], [])
+    assert_equals(parser.parsed_metar["sky_condition"], ['FEW032', 'OVC041'])
+    assert_equals(parser.parsed_metar["temp"], '06')
+    assert_equals(parser.parsed_metar["dewpoint"], '05')
+    assert_equals(parser.parsed_metar["altimeter"], 'A3017')
+    assert_equals(parser.parsed_metar["remarks"], 'RMK AO2 RAB35E44 SLP219 P0000 T00560050')
+
+  ### Test Individual Token Processors
+  def test_parse_metar_header_with_metar_header(self):
+    parser = MetarParser()
+    tokens = 'METAR KHIO'.split()
+    res = parser.parse_metar_header(tokens)
+    assert_equals(res[0], 'KHIO')
+
+  def test_parse_metar_header_without_metar_header(self):
+    parser = MetarParser()
+    tokens = 'KHIO 290653Z'.split()
+    res = parser.parse_metar_header(tokens)
+    assert_equals(res, ['KHIO', '290653Z'])
+
+  def test_parse_icao_id(self):
+    parser = MetarParser()
+    tokens = 'KHIO 290653Z'.split()
+    res = parser.parse_icao_id(tokens)
+    assert_equals(parser.parsed_metar["icao_id"], 'KHIO')
+    assert_equals(res[0], '290653Z')
+
+  def test_parse_datetime(self):
+    parser = MetarParser()
+    tokens = '290653Z AUTO'.split()
+    res = parser.parse_datetime(tokens)
+    assert_equals(parser.parsed_metar["obs_datetime"], '290653Z')
+    assert_equals(res[0], 'AUTO')
+
+  def test_parse_automated_obs_flag_with_flag(self):
+    parser = MetarParser()
+    tokens = 'AUTO 00000KT'.split()
+    res = parser.parse_automated_obs_flag(tokens)
+    assert_equals(parser.parsed_metar["mod_auto"], True)
+    assert_equals(res[0], '00000KT')
+
+  def test_parse_automated_obs_flag_without_flag(self):
+    ### If the AUTO flag is missing from the METAR, the obs_datetime
+    ### field should be false and nothing stripped from the METAR
+    parser = MetarParser()
+    tokens = '290653Z 00000KT'.split()
+    res = parser.parse_automated_obs_flag(tokens)
+    assert_equals(parser.parsed_metar["mod_auto"], False)
+    assert_equals(res, ['290653Z', '00000KT'])
+
+  def test_parse_winds_calm(self):
+    parser = MetarParser()
+    tokens = '00000KT 10SM'.split()
+    res = parser.parse_wind_dir_speed(tokens)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '00000KT')
+    assert_equals(res, ['10SM'])
+
+  def test_parse_winds_light_variable(self):
+    parser = MetarParser()
+    tokens = 'VRB005KT'.split()
+    res = parser.parse_wind_dir_speed(tokens)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], 'VRB005KT')
+
+  def test_parse_winds_basic(self):
+    parser = MetarParser()
+    tokens = '18004KT 10SM'.split()
+    res = parser.parse_wind_dir_speed(tokens)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '18004KT')
+
+  def test_parse_winds_with_gusts(self):
+    parser = MetarParser()
+    tokens = '21016G24KT 10SM'.split()
+    res = parser.parse_wind_dir_speed(tokens)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '21016G24KT')
+
+  def test_parse_winds_variable(self):
+    parser = MetarParser()
+    tokens = '18015KT 150V210 10SM'.split()
+    # Make sure both the wind info and direction variation get picked up
+    res = parser.parse_wind_dir_speed(tokens)
+    res = parser.parse_wind_dir_variation(res)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '18015KT')
+    assert_equals(parser.parsed_metar["wind_dir_variation"], '150V210')
+
+  def test_parse_winds_variable_missing(self):
+    ### If the variable winds section is missing we shouldn't set the
+    ### MetarParser's wind_dir_variation field, or strip anything
+    parser = MetarParser()
+    tokens = '18015KT 10SM'.split()
+    # Include the wind_dir_speed parseor for a sanity check
+    res = parser.parse_wind_dir_speed(tokens)
+    res = parser.parse_wind_dir_variation(res)
+    assert_equals(parser.parsed_metar["wind_dir_speed"], '18015KT')
+    assert_equals(parser.parsed_metar["wind_dir_variation"], '')
+    assert_equals(res, ['10SM'])
+
+  def test_parse_visibility(self):
+    parser = MetarParser()
+    tokens = '10SM R11/P6000FT'.split()
+    res = parser.parse_visibility(tokens)
+    assert_equals(parser.parsed_metar["vis"], '10SM')
+    assert_equals(res[0], 'R11/P6000FT')
+
+  def test_parse_visibility_missing(self):
+    ### If the visibility field is missing, the MetarParser's 'vis' field
+    ### should remain unset and nothing stripped from the METAR
+    parser = MetarParser()
+    tokens = '21016G24KT R11/P6000FT'.split()
+    res = parser.parse_visibility(tokens)
+    assert_equals(parser.parsed_metar["vis"], '')
+    assert_equals(res, ['21016G24KT', 'R11/P6000FT'])
+
+  def test_parse_rvr(self):
+    parser = MetarParser()
+    tokens = 'R11/P6000FT -RA BR'.split()
+    res = parser.parse_rvr(tokens)
+    assert_equals(parser.parsed_metar["rvr"], 'R11/P6000FT')
+    assert_equals(res, ['-RA', 'BR'])
+
+  def test_parse_rvr_missing(self):
+    ### If the RVR is missing, the MetarParser's 'rvr' field should
+    ### remain unset and nothing stripped from the METAR
+    parser = MetarParser()
+    tokens = 'VRB005 -RA BR'.split()
+    res = parser.parse_rvr(tokens)
+    assert_equals(parser.parsed_metar["rvr"], '')
+    assert_equals(res, ['VRB005', '-RA', 'BR'])
+
+  def test_parse_rvr_missing_with_similar_wx_phenomena(self):
+    ### The RVR field begins with 'R', but the following WX phenomena
+    ### section can also start with 'R' in the case of moderate rain: 'RA'.
+    ### This test ensures we don't mis-identify 'RA' as the RVR field.
+    parser = MetarParser()
+    tokens = '18015KT RA'.split()
+    res = parser.parse_wind_dir_speed(tokens)
+    res = parser.parse_rvr(res)
+    assert_equals(parser.parsed_metar["rvr"], '')
+    assert_equals(res, ['RA'])
+
+  def test_parse_wx_phenomena(self):
+    parser = MetarParser()
+    tokens = '-RA BR BKN015 OVC025'.split()
+    res = parser.parse_wx_phenomena(tokens)
+    assert_equals(parser.parsed_metar["wx_phenomena"], ['-RA', 'BR'])
+    assert_equals(res, ['BKN015', 'OVC025'])
+
+  def test_parse_wx_phenomena_missing(self):
+    parser = MetarParser()
+    tokens = 'FEW020 SCT150'.split()
+    res = parser.parse_wx_phenomena(tokens)
+    assert_equals(parser.parsed_metar["wx_phenomena"], [])
+    assert_equals(res, ['FEW020', 'SCT150'])
+
+  def test_parse_sky_condition(self):
+    parser = MetarParser()
+    tokens = 'BKN015 OVC025 06/04'.split()
+    res = parser.parse_sky_condition(tokens)
+    assert_equals(parser.parsed_metar["sky_condition"], ['BKN015', 'OVC025'])
+    assert_equals(res, ['06/04'])
+
+  def test_parse_sky_condition_missing(self):
+    parser = MetarParser()
+    tokens = '06/04 A2990'.split()
+    res = parser.parse_sky_condition(tokens)
+    assert_equals(parser.parsed_metar["sky_condition"], [])
+    assert_equals(res, ['06/04', 'A2990'])
+
+  def test_parse_tmp_dewpoint(self):
+    parser = MetarParser()
+    tokens = '06/04 A2990'.split()
+    res = parser.parse_temp_dewpoint(tokens)
+    assert_equals(parser.parsed_metar["temp"], '06')
+    assert_equals(parser.parsed_metar["dewpoint"], '04')
+    assert_equals(res, ['A2990'])
+
+  def test_parse_temp_dewpoint_missing(self):
+    parser = MetarParser()
+    tokens = 'A2990 RMK'.split()
+    res = parser.parse_temp_dewpoint(tokens)
+    assert_equals(parser.parsed_metar["temp"], '')
+    assert_equals(parser.parsed_metar["dewpoint"], '')
+    assert_equals(res, ['A2990', 'RMK'])
+
+  def test_parse_altimeter(self):
+    parser = MetarParser()
+    tokens = 'A2990 RMK'.split()
+    res = parser.parse_altimeter(tokens)
+    assert_equals(parser.parsed_metar["altimeter"], 'A2990')
+    assert_equals(res, ['RMK'])
+
+  def test_parse_altimeter_missing(self):
+    parser = MetarParser()
+    tokens = 'RMK TORNADO'.split()
+    res = parser.parse_altimeter(tokens)
+    assert_equals(parser.parsed_metar["altimeter"], '')
+    assert_equals(res, ['RMK', 'TORNADO'])
+
+  @classmethod
+  def teardown_class(cls):
+    pass
